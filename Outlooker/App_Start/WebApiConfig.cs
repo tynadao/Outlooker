4c2a1b5efddecd41fb7878868c322b11a24078b1
Tyna Dao <tyna.dao@aarons.com>
2016-08-04 11:12:58 -0400

    Building the API: Formatters

--- Outlooker/App_Start/WebApiConfig.cs
+++ Outlooker/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace Outlooker
@@ -9,6 +11,9 @@ namespace Outlooker
     {
         public static void Register(HttpConfiguration config)
         {
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/v1/{controller}/{id}",
