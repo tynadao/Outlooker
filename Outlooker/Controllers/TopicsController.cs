34367be874b4b88ada026c7070279244de34aea4
Tyna Dao <tyna.dao@aarons.com>
2016-08-04 12:51:29 -0400

    Building the API: Inserting a Topic via the API

--- Outlooker/Controllers/TopicsController.cs
+++ Outlooker/Controllers/TopicsController.cs
@@ -23,5 +23,20 @@ namespace Outlooker.Controllers
 
             return topics;
         }
+
+        public HttpResponseMessage Post([FromBody]Topic newTopic)
+        {
+            if(newTopic.Created == default(DateTime))
+            {
+                newTopic.Created = DateTime.UtcNow;
+            }
+
+            if(_repo.AddTopic(newTopic) && _repo.Save())
+            {
+                return Request.CreateResponse(HttpStatusCode.Created, newTopic);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.BadRequest);
+        }
     }
 }
