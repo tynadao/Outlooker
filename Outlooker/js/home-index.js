8cfa74d8792a5b0e1bfb9b3ccef5497e9cae55d8
Tyna Dao <tyna.dao@aarons.com>
2016-08-05 09:57:37 -0400

    AngularJSBasics: Data Binding (use ng-bind instead of expressions for faster loading)

--- Outlooker/js/home-index.js
+++ Outlooker/js/home-index.js
@@ -1,4 +1,29 @@
 ﻿//home-index.js
-(function homeIndexController() {
-    alert("inside the controller");
-})();
+
+//deviation from pluralsight tutorial. use references here for initial creation of module and controller:
+//http://www.infragistics.com/community/blogs/dhananjay_kumar/archive/2015/05/13/how-to-use-angularjs-in-asp-net-mvc-and-entity-framework-4.aspx
+//http://stackoverflow.com/questions/19408011/angularjs-error-argument-firstctrl-is-not-a-function-got-undefined
+
+
+homeIndexModule = angular
+    .module('HomeIndexModule', [])
+    .controller('homeIndexController', function ($scope) {
+        $scope.name = "Shawn Wildermuth";
+        $scope.data = [
+            {
+                title: "This is a message",
+                body: "I am right about this.",
+                created: "4/24/2013"
+            },
+            {
+                title: "This is a second message",
+                body: "I am right about this.",
+                created: "4/24/2013"
+            },
+            {
+                title: "This is a third message",
+                body: "I am right about this.",
+                created: "4/24/2013"
+            },
+        ];
+    });
